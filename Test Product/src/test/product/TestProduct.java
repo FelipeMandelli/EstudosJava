86bd19bf362e3086abdd8af51f2447c1f51cff41
Felipe Fernandes Mandelli <85856091+FelipeMandelli@users.noreply.github.com>
2022-03-02 21:30:07 -0300

    Comentando TestProduct

--- Test Product/src/test/product/TestProduct.java
+++ Test Product/src/test/product/TestProduct.java
@@ -16,13 +16,15 @@ public class TestProduct {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Gera Um Arraylist para Armazenar os Objetos
         ArrayList<Produto> produtos = new ArrayList();
         
+        // Adiciona Objetos ao Array
         produtos.add (new Produto("Monitor", 2000));
         produtos.add (new Produto("Teclado", 500));
         produtos.add (new Produto("Mouse", 200));
         
-                
+        // Loop Percorrendo o Array e Apresentando os Atributos dos Objetos  
         for(Produto p: produtos){
             System.out.println("Nome do produto:\t" + p.getName());
             System.out.println("Status do produto:\t" + p.getStatus());

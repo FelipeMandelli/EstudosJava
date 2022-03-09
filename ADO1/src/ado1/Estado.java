db0757d34384d20effc6bed295eeb92a8c817c9a
Felipe Fernandes Mandelli <85856091+FelipeMandelli@users.noreply.github.com>
2022-03-09 16:44:27 -0300

    Update Estado.java

--- ADO1/src/ado1/Estado.java
+++ ADO1/src/ado1/Estado.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ado1;
 
 /**
  *
- * @author Desktop
+ * @author Felipe Mandelli
+ * Date: 09/03/2022
  */
 public class Estado {
     
